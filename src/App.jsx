8bb78ba8e39919798dcd2dab833da5d3ffa95b97
dkg08 <dilipkumargiri.23@gmail.com>
2023-12-30 19:12:43 +0545

    better welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import logo from "./assets/react.jpg";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello, from DKG.</h1>
-      <h2>Welcome to </h2>
+    <div className="welcome-container">
+      <h1 className="welcome-heading">Welcome, fellow developer!</h1>
+      <p className="welcome-message">We're glad to have you here.</p>
+      <img src={logo} alt="react-logo" style={{ width: 500, height: 300 }} />
     </div>
   );
 }
